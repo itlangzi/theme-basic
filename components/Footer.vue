e7072c2ed341fd203dac194bf77613c9c07aa873
itlangzi <573542922@qq.com>
2025-09-15 10:19:55 +0800

    fix:copyright

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-screen-xl py-4">
       <div class="text-gray-500 text-sm text-center">
         Powered by
-        <a target="_blank" href="https://github.com/iDataRiver/theme-basic" class="underline">@iDataRiver.</a>
+        <a target="_blank" href="https://mhxiaoshen.vip" class="underline">@漫画小生</a>
       </div>
     </div>
   </footer>
